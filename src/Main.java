1fb2800f6f1201452a093e3be1ae6eabe55a6b46
Alireza Akbari <AliAMQ@users.noreply.github.com>
2018-05-04 20:18:46 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,62 +5,38 @@ and the maximum number of consecutive 1's in the binary representation.
 import java.util.ArrayList;
 
 public class Main {
+   public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        int number = 35;
-        System.out.println(number + " in binary will be:");
-        ArrayList<Boolean> binaryList =toBinary(number);
-        printBinary(binaryList);
-        System.out.println("With max " + numberOfConsecutiveOnes(binaryList) + " consecutive ones");
-    }
+        int nmbr = 35;
 
-    public static ArrayList<Boolean> toBinary(int n){
-        int remainder = 0;
-        ArrayList<Boolean> tmpList = new ArrayList<Boolean>();
-        while (n>0){
-            if(n%2==0){
-                tmpList.add(false);
+        Stack<Integer> stack = new Stack();
+
+
+        while ((nmbr/2)>=1){
+            stack.push(nmbr%2);
+            nmbr = nmbr/2;
+        }
+
+        stack.push(nmbr);
+
+        int iterations = stack.size();
+
+        int cntr = 0;
+        int max=0;
+
+        for (int i =0;i<iterations;i++){
+            int tmp = stack.pop();
+            if (tmp==1){
+                cntr++;
+                if (max < cntr) max = cntr;
             }
             else{
-                tmpList.add(true);
+                cntr=0;
             }
-            n= n/2;
-        }
-        ArrayList<Boolean> binaryList = new ArrayList<Boolean>();
-        for (int i = tmpList.size() -1; i>=0; i--){
-            binaryList.add(tmpList.get(i));
-        }
-        return binaryList;
-    }
-
-    public static void printBinary(ArrayList<Boolean> binaryList){
-        for (int i=0;i<binaryList.size();i++){
-            if(binaryList.get(i)==true){
-                System.out.print(1);
-            }
-            else{
-                System.out.print(0);
-            }
+            System.out.print(tmp);
         }
         System.out.println();
-    }
+        System.out.println(max);
 
-    public static int numberOfConsecutiveOnes(ArrayList<Boolean> binaryList){
-
-        int consecutives= 0;
-        int counter=0;
-
-        for (int i = 0; i<binaryList.size(); i++){
-            if (binaryList.get(i)==true) {
-                counter++;
-                if (consecutives < counter){
-                    consecutives = counter;
-                }
-            }
-            else{
-                counter = 0;
-            }
-        }
-        return consecutives;
     }
 }
