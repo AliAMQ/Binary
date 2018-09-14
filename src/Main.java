f923384dcef8616ef67cfd83e19a3e611f901df3
Alireza Akbari <alireza.amq@gmail.com>
2018-09-14 14:27:10 -0400

    fixed input

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,11 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
 
-       System.out.println("Enter the number:");
+       /*System.out.println("Enter the number:");
        Scanner in = new Scanner(System.in);
+        int nmbr = in.nextInt();*/
 
-        int nmbr = in.nextInt();
+       int nmbr = 35;
 
 
         System.out.println(getNuber(nmbr));
